2f1fe2c149aa516ee9d0069f015ae57620a0bdeb
Chidimma Okafor <chidimma.okafor.c@gmail.com>
2020-03-09 10:58:54 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace chartOfAccountMainRepoNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
